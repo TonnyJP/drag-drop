f3acbc915e6269bb6a3bd6343548df17279c6e1a
TonnyJP <tonny.piper@gmx.de>
2023-04-08 21:52:34 +0200

    displaying the form

--- prj-00-initial-starting-setup/src/app.ts
+++ prj-00-initial-starting-setup/src/app.ts
@@ -1,1 +1,58 @@
-// Code goes here!
+//autobind-decorator
+function autobind(_target: any, _mehtodName: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        get() {
+            const boundFn = originalMethod.bind();
+            return boundFn;
+        }
+    };
+
+    return adjDescriptor
+}
+
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement : HTMLInputElement;
+  descriptionInputElement : HTMLInputElement;
+  peopleInputElement : HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  @autobind
+  private submitHandler (event: Event ) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value)
+  }
+  private configure () {
+    this.element.addEventListener('submit', this.submitHandler.bind(this))
+  }
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    console.log(this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
